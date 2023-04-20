05a0a8e2d8e25b67085041a3a1cecb67c89d13c5
emmabaka <bakaemma88@gmail.com>
2023-04-20 16:33:24 +0300

    new commit

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -31,6 +31,6 @@ ContactList.propTypes = {
       name: PropTypes.string.isRequired,
       number: PropTypes.string.isRequired,
     })
-  ),
-  deleteContact: PropTypes.func,
+  ).isRequired,
+  deleteContact: PropTypes.func.isRequired,
 };
